9d91b3e2d17591fe609f0c1ddf9dd00c10139964
Cho Chang <cyleung.berkeley@gmail.com>
2016-09-08 19:40:00 -0700

    fix materialize

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -1,9 +1,15 @@
 angular.module("jwt_auth")
 	.controller("HomeController", HomeController)
 
-HomeController.$inject = ['$http', '$state']
+HomeController.$inject = ['$http', '$state', '$scope']
 
-function HomeController ($http, $state) {
+function HomeController ($http, $state, $scope) {
 	var self = this
 	console.log("Home Controller")
+
+		$scope.$on('$viewContentLoaded', function(){
+			console.log("content loaded")
+				$('.parallax').parallax();
+
+ });
 }
